3af0a1c73ed45cffdac149107deff0555b2b1d5a
Reza A'masyi <mnurrreza@gmail.com>
2021-11-10 15:24:06 +0700

    time(01:03:23): Instantiate `class` from a `csv` file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import csv
+
+
 class Item:
     pay_rate = 0.8  # The pay rate after 20% Discount
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
@@ -11,19 +15,31 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("items.csv", "r") as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
 
-item1 = Item("Phone", 100, 8)
-item1.apply_discount()
-print(item1.price)
+        for item in items:
+            Item(
+                name=item.get("name"),
+                price=float(item.get("price")),
+                quantity=int(item.get("quantity")),
+            )
+
+    def __repr__(self):
+        return f"Item('{self.name }', '{self.price}', '{self.quantity}')"
 
 
-item2 = Item("Laptop", 1000, 5)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+Item.instantiate_from_csv()
+print(Item.all)
