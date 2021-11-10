eb01161af8a246eb4bc31ed4938347e3f13b2fcd
Reza A'masyi <mnurrreza@gmail.com>
2021-11-10 18:45:56 +0700

    time(02:06:05): Inheritance => Reusing methods and attributes
    Reusing methods and attributes from the parent-class/superclass on
    child-classes/subclass. In this case all methods that are defined on `class
    Item` could be used in `class Phone`, while there are also specific methods that
    are defined only on `class Phone` won't interfene with the parent-class.
    
    Remember: Certain methods or attributes could be defined in the subclass to
    override the defined methods and attributes in the superclass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from item import Item
+from phone import Phone
 
-item1 = Item("MyItem", 750)
+item1 = Phone("jscPhone", 750, 5)
 
-item1.send_email()
+item1.apply_increment(0.33)
+
+print(item1.price)
