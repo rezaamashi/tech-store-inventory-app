4bcf6b88fc49bdc2a2429434fa95b41356344f47
Reza A'masyi <mnurrreza@gmail.com>
2021-11-10 14:54:31 +0700

    time(00:42:05): `class` vs `instance` attributes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Item:
+    pay_rate = 0.8  # The pay rate after 20% Discount
+
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} must not be negative or zero"
@@ -12,11 +14,16 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
 
 item1 = Item("Phone", 100, 8)
+item1.apply_discount()
+print(item1.price)
 
-item2 = Item("Laptop", 1000, 3)
 
-
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+item2 = Item("Laptop", 1000, 5)
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
